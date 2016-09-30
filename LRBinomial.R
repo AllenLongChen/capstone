1a328172e833fdf5db17f8c093f8303ccc3ea9ed
Meng Wang <maggiewm.pku@gmail.com>
2016-09-29 22:26:17 -0400

    09_29_16_1
    modified test.R
    added LRBinomial.R

---
+++ LRBinomial.R
@@ -0,0 +1,125 @@
+fh <- function(z, n)
+{
+  return (0.5 + sign(z)*sqrt(0.25-0.25*exp(-((z/(n+1/3+0.1/(n+1)))^2)*(n+1/6))))
+}
+
+binPriceLR <- function (X, S0, r, sig, Texp, oType, earlyExercise=TRUE, steps=10000)
+{
+  # Function to calculate the price of a vanilla European or American
+  # Put or Call option using a Leisen-Reimer binomial tree.
+  #
+  # Inputs: X - strike
+  #       : S0 - stock price
+  #       : r - risk free interest rate
+  #       : sig - volatility
+  #       : Texp -  Time to expiry
+  #       : steps - number of time steps to calculate
+  #       : oType - must be 'PUT' or 'CALL'.
+  #       : earlyExercise - true for American, false for European.
+  #
+  # Output: oPrice - the option price
+  #
+  
+  # Calculate the d1 and d2 parameters of the Black-Scholes model
+  #T <- dt*steps # time to maturity
+  
+  nK <- length(X)
+  T <- Texp
+  dt <- Texp / steps
+  
+  # Calculate the Leisen-Reimer model parameters
+  # The parameter n must be odd
+  n <- 0
+  if((steps %% 2) > 0)
+  {
+    n <- steps
+  }
+  else
+  {
+    n <- steps+1
+  }
+  
+  V <- numeric(nK)
+  
+  for(i in 1:nK){
+    d1 <- (log(S0/X[i])+(r+sig[i]*sig[i]/2)*T)/sig[i]/sqrt(T);
+    d2 <- (log(S0/X[i])+(r-sig[i]*sig[i]/2)*T)/sig[i]/sqrt(T);
+
+    pbar <- fh(d1,n)
+    p <- fh(d2,n)
+    u <- exp(r*dt)*pbar/p
+    d <- (exp(r*dt)-p*u)/(1-p)
+  
+    # Loop over each node and calculate the CRR underlying price tree
+    priceTree <- array(dim=c(steps+1,steps+1))
+    priceTree[1,1] <- S0;
+  
+    for (idx in 2:(steps+1) )
+    {
+      priceTree[1:(idx-1),idx] <- priceTree[1:(idx-1),(idx-1)] * u;
+      priceTree[idx,idx] <- priceTree[(idx-1),(idx-1)]*d;
+    }
+  
+    # Calculate the value at expiry
+    valueTree <- array(dim=c(steps+1,steps+1))
+    
+    if(oType == 'P')
+    {
+      valueTree[,] <- pmax(X[i]-priceTree[,],0)
+    }
+    else if(oType == 'C')
+    {
+      valueTree[,] <- pmax(priceTree[,]-X[i],0,na.rm = TRUE)
+    }
+    
+    #Loop backwards to get values at the earlier times
+    #steps = size(priceTree,2)-1;
+    for (idx in steps:1)
+    {
+      valueTree[1:idx,idx] <- exp(-r*dt)*(p*valueTree[1:idx,(idx+1)] + (1-p)*valueTree[2:(idx+1),(idx+1)])
+      if (earlyExercise == TRUE)
+      {
+        if(oType == 'P')
+        {
+          valueTree[1:idx,idx] <- pmax(X[i]-priceTree[1:idx,idx], valueTree[1:idx,idx], na.rm = TRUE)
+        }
+        else if(oType == 'C')
+        {
+          valueTree[1:idx,idx] <- pmax(priceTree[1:idx,idx]-X[i], valueTree[1:idx,idx], na.rm = TRUE)
+        }
+      }
+    }
+
+    V[i] <- (valueTree[1,1])
+  }
+  return (V)  
+}
+
+
+# This function now works with vectors of strikes and option values
+binImpVolLR <- function(S0, K, T, r, V, oType, earlyExercise, steps=10000)
+{
+  nK <- length(K);
+  sigmaL <- rep(1e-10,nK);
+  
+  VL <- binPriceLR(K, S0, r, sigmaL, T, oType, earlyExercise, steps)
+  
+  sigmaH <- rep(10,nK);
+  VH <- binPriceLR(K, S0, r, sigmaH, T, oType, earlyExercise, steps)
+  
+  while (mean(sigmaH - sigmaL) > 1e-10)
+  {
+    sigma <- (sigmaL + sigmaH)/2;
+    VM <- binPriceLR(K, S0, r, sigma, T, oType, earlyExercise, steps)
+    
+    VL <- VL + (VM < V)*(VM-VL);
+    sigmaL <- sigmaL + (VM < V)*(sigma-sigmaL);
+    VH <- VH + (VM >= V)*(VM-VH);
+    sigmaH <- sigmaH + (VM >= V)*(sigma-sigmaH);
+  }
+  return(sigma);
+}
+
+
+
+
--- test.R
+++ test.R
@@ -1,6 +1,9 @@
+setwd("/Users/mengwang/Documents/Capstone")
 source("BlackScholes.R")
 source("localVolMC.R")
+source("localVolPDE.R")
 source("localVolDupirePDE.R")
+source("LRBinomial.R")
 
 ### set local vol parameters
 params <- list(a=0.0012,b=0.1634,sig=0.1029,rho=-0.5555,m1=0.0439)
@@ -8,7 +11,7 @@ params <- list(a=0.0012,b=0.1634,sig=0.1029,rho=-0.5555,m1=0.0439)
 
 ### local vol MC
 strikes <- c(0.8,1.0,1.2) 
-print(LocalVolMC(params)(S0=1, capT=1, strikes, N=100000, m=16, evolve=evolveEuler, exactVols=NULL))
+print(LocalVolMC(params)(S0=1, capT=1, strikes, N=1000000, m=16, evolve=evolveEuler, exactVols=NULL))
 
 ### local vol Dupire PDE
 sigmaLocalVolGeneric <- function(params) {
@@ -26,4 +29,24 @@ sigmaLocalVolGeneric <- function(params) {
 
 sigmaLocalVol<-sigmaLocalVolGeneric(params)
 
-print(callLocalVolDupirePDE(S0=1, r=0, q=0, sigmaLocalVol, t=1, dK=0.001, dt=0.001, sdw=4, start=1, theta=1/2))
+callPricePDE<-numeric(3)
+impliedVolPDE<-numeric(3)
+callPriceLR<-numeric(3)
+impliedVolLR<-numeric(3)
+for (i in 1:3){
+  strike=strikes[i]
+  callPricePDE[i]<-callLocalVolDupirePDE(S0=1,K=strike,r=0, q=0, sigmaLocalVol, t=1, dK=0.01, dt=0.01, sdw=4, start=1, theta=1/2)
+  #callPricePDE[i]<-callLocalVolPDE(S0=1,K=strike,r=0, q=0, sigmaLocalVol, t=1, dS=0.01, dt=0.01, sdw=4, start=1, theta=1/2)
+  #impliedVolPDE[i]<-BSImpliedVolCall(S0=1, K=strike, capT=1, r=0, C=callPricePDE[i])
+  callPriceLR[i]<-binPriceLR(X=strike, S0=1, r=0, sigmaLocalVol, Texp=1, oType="C", earlyExercise=FALSE, steps=100)
+  #impliedVolLR[i]<-binImpVolLR(S0=1, K=strike, capT=1, r=0, V=callPriceLR[i], oType="C", earlyExercise=FALSE, steps=10)
+}
+
+print(callPricePDE)
+#print(impliedVolPDE)
+print(callPriceLR)
+#print(impliedVolLR)
+
+print(BSFormula(S0=1, K=strikes, capT=1, r=0, sigma=0.2))
+
+
